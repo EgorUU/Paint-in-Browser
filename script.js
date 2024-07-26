66576927910ac678f1b87eb2fc23efa53baf46c6
EgorUU <egorsairanov12@gmail.com>
2024-07-27 00:02:54 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const canvas = document.getElementById('canvas')
+let ctx = canvas.getContext('2d')
+const choice = document.querySelectorAll('.choices>div')
+let color = ''
+
+choice.forEach((c) => {
+    c.addEventListener('click', () => {
+        choice.forEach((c) => {
+            c.classList.remove('active')
+        })
+        c.classList.add('active')
+        color = getComputedStyle(c).backgroundColor
+        ctx.beginPath()
+    })
+})
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight - 30
+let draw = false
+ctx.lineWidth = 10 * 2;
+canvas.addEventListener('mousemove', (e) => {
+    if(draw) {
+        ctx.lineTo(e.clientX, e.clientY - 34)
+        ctx.stroke()
+
+        ctx.beginPath()
+        ctx.arc(e.clientX, e.clientY - 34, 10,0, Math.PI * 2)
+        ctx.fill()
+
+        ctx.beginPath()
+        ctx.moveTo(e.clientX, e.clientY - 34)
+    }
+})
+canvas.addEventListener('mousedown', (e) => {
+    draw = true
+    console.log(draw)
+})
+canvas.addEventListener('mouseup', () => {
+    draw = false
+    console.log(draw)
+    ctx.beginPath()
+})
