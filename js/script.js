8eb891a96cd86c6866ffc2fcaf34691f6e711c4f
EgorUU <egorsairanov12@gmail.com>
2024-07-28 18:34:19 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -9,7 +9,15 @@ choice.forEach((c) => {
             c.classList.remove('active')
         })
         c.classList.add('active')
+        if (getComputedStyle(c).backgroundColor == 'rgb(255, 255, 255)') {
+            ctx.lineWidth = 20 * 2;
+        }
+        else {
+            ctx.lineWidth = 10 * 2;
+        }
         color = getComputedStyle(c).backgroundColor
+        ctx.fillStyle = color
+        ctx.strokeStyle = color
         ctx.beginPath()
     })
 })
@@ -22,11 +30,9 @@ canvas.addEventListener('mousemove', (e) => {
     if(draw) {
         ctx.lineTo(e.clientX, e.clientY - 34)
         ctx.stroke()
-
         ctx.beginPath()
         ctx.arc(e.clientX, e.clientY - 34, 10,0, Math.PI * 2)
         ctx.fill()
-
         ctx.beginPath()
         ctx.moveTo(e.clientX, e.clientY - 34)
     }
